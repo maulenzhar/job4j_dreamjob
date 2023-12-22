3fa0e7c8ce3a69b4dfdd426a64d647f36d92313e
maulen <example@example.com>
2023-12-22 13:13:23 +0600

    первый коммит

---
+++ src/main/java/ru/job4j/dreamjob/Job4jDreamjobApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.dreamjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jDreamjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jDreamjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/job4j/dreamjob/Job4jDreamjobApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.dreamjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jDreamjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
