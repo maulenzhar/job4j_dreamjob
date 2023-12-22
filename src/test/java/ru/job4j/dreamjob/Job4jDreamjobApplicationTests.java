8f42b9268085f4728fa2dae131345aa8f3c5dc17
maulen <example@example.com>
2023-12-22 16:26:58 +0600

    Реализован "Hello World!

--- src/test/java/ru/job4j/dreamjob/Job4jDreamjobApplicationTests.java
+++ src/test/java/ru/job4j/dreamjob/Job4jDreamjobApplicationTests.java
@@ -1,13 +1,13 @@
 package ru.job4j.dreamjob;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Job4jDreamjobApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Job4jDreamjobApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
