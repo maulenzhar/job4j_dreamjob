3e00946628545b4c93b1402949bb1c13ccc06aa8
maulen <example@example.com>
2023-12-22 16:30:26 +0600

    Реализован "Hello World 2!

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,16 +1,16 @@
 package ru.job4j.dreamjob.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
-        return "Hello World!";
+        return "index";
     }
 
 }
