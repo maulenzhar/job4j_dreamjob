faa30d66148093f957533d6486582aa02a3b1814
maulen <example@example.com>
2023-12-23 00:58:14 +0600

    Список кандидатов

--- src/main/java/ru/job4j/dreamjob/repository/CandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/CandidateRepository.java
@@ -5,7 +5,7 @@ import ru.job4j.dreamjob.model.Candidate;
 import java.util.Collection;
 import java.util.Optional;
 
-public interface CandidateRepository {
+public interface CandidateRepository{
     Candidate save(Candidate candidate);
 
     void deleteById(int id);
