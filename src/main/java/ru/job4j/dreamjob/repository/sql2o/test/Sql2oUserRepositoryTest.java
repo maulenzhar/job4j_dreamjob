e5f394dadde7f5631233e5bdffebe366c06d6c14
maulen <example@example.com>
2024-01-08 03:36:09 +0600

    Многопоточность в базе данных

--- src/main/java/ru/job4j/dreamjob/repository/sql2o/test/Sql2oUserRepositoryTest.java
+++ src/main/java/ru/job4j/dreamjob/repository/sql2o/test/Sql2oUserRepositoryTest.java
@@ -13,7 +13,6 @@ import java.util.Properties;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class Sql2oUserRepositoryTest {
     private static Sql2oUserRepository sql2oUserRepository;
 
